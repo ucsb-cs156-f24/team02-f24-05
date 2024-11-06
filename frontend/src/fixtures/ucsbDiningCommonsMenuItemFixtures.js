aa493c208f20514efad99231711d172313912b15
Alex Castelein <alex.castelein@gmail.com>
2024-11-05 17:35:09 -0800

    attempted fixture format fix

--- frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -1,31 +1,30 @@
 const ucsbDiningCommonsMenuItemFixtures = {
-    oneMenuItem: {
+  oneMenuItem: {
+    id: 1,
+    diningCommonsCode: "ortega",
+    name: "Sandwich",
+    station: "Deli",
+  },
+  threeMenuItems: [
+    {
       id: 1,
       diningCommonsCode: "ortega",
-      name: "Sandwich",
-      station: "Deli",
+      name: "Broccoli",
+      station: "Sides",
     },
-    threeMenuItems: [
-      {
-        id: 1,
-        diningCommonsCode: "ortega",
-        name: "Broccoli",
-        station: "Sides",
-      },
-      {
-        id: 2,
-        diningCommonsCode: "portola",
-        name: "Banana",
-        station: "Fruits",
-      },
-      {
-        id: 3,
-        diningCommonsCode: "portola",
-        name: "Cheeseburger",
-        station: "Entrees",
-      },
-    ],
-  };
-  
-  export { ucsbDiningCommonsMenuItemFixtures };
-  
+    {
+      id: 2,
+      diningCommonsCode: "portola",
+      name: "Banana",
+      station: "Fruits",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "portola",
+      name: "Cheeseburger",
+      station: "Entrees",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
