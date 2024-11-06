ac1b66a22549a023a1d12c021c7f2025ad8644a0
Alex Castelein <alex.castelein@gmail.com>
2024-11-05 17:19:24 -0800

    added fixtures file for DCMI

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneMenuItem: {
+      id: 1,
+      diningCommonsCode: "ortega",
+      name: "Sandwich",
+      station: "Deli",
+    },
+    threeMenuItems: [
+      {
+        id: 1,
+        diningCommonsCode: "ortega",
+        name: "Broccoli",
+        station: "Sides",
+      },
+      {
+        id: 2,
+        diningCommonsCode: "portola",
+        name: "Banana",
+        station: "Fruits",
+      },
+      {
+        id: 3,
+        diningCommonsCode: "portola",
+        name: "Cheeseburger",
+        station: "Entrees",
+      },
+    ],
+  };
+  
+  export { ucsbDiningCommonsMenuItemFixtures };
+  
