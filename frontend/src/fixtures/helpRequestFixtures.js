ba8e4f20a989b5e3c84923477f7ac871e844bddc
Ryan Zanone <ryanzanone@ucsb.edu>
2024-11-05 17:34:02 -0800

    fixed formatting

--- frontend/src/fixtures/helpRequestFixtures.js
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -1,5 +1,15 @@
 const helpRequestFixtures = {
-    oneRequest: {
+  oneRequest: {
+    id: 1,
+    requesterEmail: "ryanzanone@ucsb.edu",
+    teamId: "5",
+    tableOrBreakoutRoom: "5",
+    requestTime: "2022-01-02T12:00:00",
+    explanation: "explanation",
+    solved: true,
+  },
+  threeRequests: [
+    {
       id: 1,
       requesterEmail: "ryanzanone@ucsb.edu",
       teamId: "5",
@@ -8,35 +18,25 @@ const helpRequestFixtures = {
       explanation: "explanation",
       solved: true,
     },
-    threeRequests: [
-      {
-        id: 1,
-        requesterEmail: "ryanzanone@ucsb.edu",
-        teamId: "5",
-        tableOrBreakoutRoom: "5",
-        requestTime: "2022-01-02T12:00:00",
-        explanation: "explanation",
-        solved: true,
-      },
-      {
-        id: 2,
-        requesterEmail: "ryanzanone@gmail.com",
-        teamId: "6",
-        tableOrBreakoutRoom: "6",
-        requestTime: "2022-01-02T12:00:00",
-        explanation: "explanation",
-        solved: false,
-      },
-      {
-        id: 3,
-        requesterEmail: "ryanzanone@cs.ucsb.edu",
-        teamId: "7",
-        tableOrBreakoutRoom: "7",
-        requestTime: "2022-01-02T12:00:00",
-        explanation: "explanation",
-        solved: false,
-      },
-    ],
-  };
+    {
+      id: 2,
+      requesterEmail: "ryanzanone@gmail.com",
+      teamId: "6",
+      tableOrBreakoutRoom: "6",
+      requestTime: "2022-01-02T12:00:00",
+      explanation: "explanation",
+      solved: false,
+    },
+    {
+      id: 3,
+      requesterEmail: "ryanzanone@cs.ucsb.edu",
+      teamId: "7",
+      tableOrBreakoutRoom: "7",
+      requestTime: "2022-01-02T12:00:00",
+      explanation: "explanation",
+      solved: false,
+    },
+  ],
+};
   
   export { helpRequestFixtures };
