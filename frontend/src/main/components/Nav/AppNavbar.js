833bc5bfa1b04385e368faed9816344249cf7e53
Baige He <baige_he@163.com>
2024-11-06 18:08:46 -0800

    added on Nav, App.js

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -69,6 +69,9 @@ export default function AppNavbar({
                   <Nav.Link as={Link} to="/placeholder">
                     Placeholder
                   </Nav.Link>
+                  <Nav.Link as={Link} to="/menu_item_review">
+                    MenuItemReview
+                  </Nav.Link>
                 </>
               ) : (
                 <></>
