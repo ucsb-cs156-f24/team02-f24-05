190983a7d45975cda6a35e2c5cbe514b3b1844a5
Baige He <baige_he@163.com>
2024-11-07 17:41:09 -0800

    Index Page for MenuItemReview plus tests and stories

--- frontend/src/main/pages/MenuItemReview/MenuItemReviewIndexPage.js
+++ frontend/src/main/pages/MenuItemReview/MenuItemReviewIndexPage.js
@@ -1,17 +1,45 @@
+import React from "react";
+import { useBackend } from "main/utils/useBackend";
+
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import MenuItemReviewTable from "main/components/MenuItemReview/MenuItemReviewTable";
+import { Button } from "react-bootstrap";
+import { useCurrentUser, hasRole } from "main/utils/currentUser";
 
 export default function MenuItemReviewIndexPage() {
-  // Stryker disable all : placeholder for future implementation
+  const currentUser = useCurrentUser();
+
+  const createButton = () => {
+    if (hasRole(currentUser, "ROLE_ADMIN")) {
+      return (
+        <Button
+          variant="primary"
+          href="/menu_item_review/create"
+          style={{ float: "right" }}
+        >
+          Create MenuItemReview
+        </Button>
+      );
+    }
+  };
+
+  const {
+    data: dates,
+    error: _error,
+    status: _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    ["/api/menu_item_review/all"],
+    { method: "GET", url: "/api/menu_item_review/all" },
+    [],
+  );
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Index page not yet implemented</h1>
-        <p>
-          <a href="/placeholder/create">Create</a>
-        </p>
-        <p>
-          <a href="/placeholder/edit/1">Edit</a>
-        </p>
+        {createButton()}
+        <h1>MenuItemReview</h1>
+        <MenuItemReviewTable dates={dates} currentUser={currentUser} />
       </div>
     </BasicLayout>
   );
